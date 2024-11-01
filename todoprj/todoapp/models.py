149f8dc52d50b3640c5e896cfe46092f9bd6ee84
Simo <153776287+SimoHypers@users.noreply.github.com>
2024-11-01 23:54:18 +0400

    update
    mostly admin tests

--- todoprj/todoapp/models.py
+++ todoprj/todoapp/models.py
@@ -1,19 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Task(models.Model):
-    STATUS_CHOICES = [
-        ('NS', 'Not Started'),
-        ('IP', 'In Progress'),
-        ('D', 'Done'),
-        ('C', 'Cancelled'),
-    ]
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
-    status = models.CharField(
-        max_length=2,
-        choices=STATUS_CHOICES,
-        default='NS',
-    )
-
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
-        return f"{self.title} - {self.get_status_display()}"
+        return self.title
+    
+    class Meta:
+        ordering = ['complete']
--- todoprj/todoapp/urls.py
+++ todoprj/todoapp/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    #declaring all the URLs right here
-    path('', views.home, name='home-page'),
-    path('add/', views.add_task, name='add-task'),
-    path('complete/<int:id>/', views.complete_task, name='complete-task'),
-    path('update_status/<int:task_id>/', views.update_status, name='update-status'),
-    path('delete-task/<int:task_id>/', views.delete_task, name='delete-task'),
+    path('', views.home, name="Home"),
+
 ]
